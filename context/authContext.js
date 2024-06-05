b3bc1c2c34edd210dc988b923d12cd8f37e075e8
sandylinl1013 <sandy.linl1013@gmail.com>
2024-06-05 22:53:21 +0800

    Part oh firebase and auth

--- context/authContext.js
+++ context/authContext.js
@@ -1,51 +1,69 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 import { createContext, useEffect, useState, useContext} from "react";
+import {auth} from "../firebaseConfig"
 
 export const AuthContext = createContext();
 
-export const AuthContextProvider = ({children})=>{
+export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(undefined)
 
-    useEffect(() =>{
-        //on AuthStateChange
-        //setTimeout(()=>{
-            setIsAuthenticated(false)
-        //},3000);
-    })
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setIsAuthenticated(true);
+                setUser(user);
+            } else {
+                setIsAuthenticated(false);
+                setUser(null);
+            }
+        });
+        return unsub;
+    }, [])
 
-    const login = async (email, password)=>{
-        try{
+    const login = async (email, password) => {
+        try {
 
-        }catch(e){
+        } catch (e) {
 
         }
     }
-    const logout = async ()=>{
-        try{
+    const logout = async () => {
+        try {
 
-        }catch(e){
-            
+        } catch (e) {
+
         }
     }
-    const register = async (email, password, username, )=>{
-        try{
+    const register = async (email, password, username,) => {
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password);
+            console.log('response.user : ', response?.user);
 
-        }catch(e){
-            
+            await setDoc(doc(db, "user", response?.user?.uid), {
+                username,
+                ProfileUrl,
+                userId: response?.user?.uid
+            })
+            return{success: true, data: response?.user};
+
+        } catch (e) {
+            return{success: false, msg: e.message};
         }
     }
 
-    return(
-        <AuthContext.Provider value={{user, isAuthenticated, login, register, logout}}>
+    return (
+        <AuthContext.Provider value={{ user, isAuthenticated, login, register, logout }}>
             {children}
         </AuthContext.Provider>
     )
 }
 
-export const useAuth = ()=>{
+export const useAuth = () => {
     const value = useContext(AuthContext);
 
-    if(!value){
+    if (!value) {
         throw new Error('useAuth must be wrapped inside AuthContextProvider');
     }
     return value;
