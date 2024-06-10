dce8758fba16962b544bce6d07feec070b8f469b
jessie-creator <62526786+jessie-creator@users.noreply.github.com>
2024-06-11 01:10:04 +0800

    Add files via upload

---
+++ context/imageContext.js
@@ -0,0 +1,8 @@
+// imageMapping.js
+const images = {
+    "photo-stickers/1.png": require('../assets/images/photo-stickers/1.png'),
+    "photo-stickers/2.png": require('../assets/images/photo-stickers/2.png'),
+    // Add all your images here
+};
+
+export default images;
