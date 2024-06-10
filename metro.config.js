f77010b8c471e0d337c12ab82ab59be89f402f2a
jessie-creator <62526786+jessie-creator@users.noreply.github.com>
2024-06-11 01:05:32 +0800

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,7 @@ const { withNativeWind } = require('nativewind/metro');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.resolver.assetExts.push("db");
+
 module.exports = withNativeWind(config, { input: './global.css' })
 
