e12fc36d494bfce0c0bc3a12089b6860e43f8100
jessie-creator <62526786+jessie-creator@users.noreply.github.com>
2024-06-11 01:08:32 +0800

    Add files via upload

--- app/(app)/_layout.js
+++ app/(app)/_layout.js
@@ -1,9 +1,17 @@
 import {View, Text } from 'react-native';
-import Reach from 'react';
+import React from 'react';
 import { Stack } from 'expo-router';
+import HomeHeader from '../../components/HomeHeader'
 
 export default function _layout(){
     return(
-        <Stack />
+        <Stack>
+            <Stack.Screen
+                name="home"
+                options={{
+                    header: ()=> <HomeHeader />
+                }}
+            />
+        </Stack>
     )
 }
