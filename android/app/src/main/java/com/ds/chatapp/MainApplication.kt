7a943aef971d0a07e52b37f3fa414824d353e02e
jessie-creator <62526786+jessie-creator@users.noreply.github.com>
2024-06-11 01:12:49 +0800

    Add files via upload

--- android/app/src/main/java/com/ds/chatapp/MainApplication.kt
+++ android/app/src/main/java/com/ds/chatapp/MainApplication.kt
@@ -15,6 +15,8 @@ import com.facebook.soloader.SoLoader
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
+import org.pgsqlite.SQLitePluginPackage
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
